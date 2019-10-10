696de6a466caceb9e80022032a9e6d5015e00291
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-10-10 13:52:19 -0500

    mixpanel setup

--- didact/src/App.js
+++ didact/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Routes from "./utils/Routes";
+import { Mixpanel } from './utils/mixpanel';
 
 
 function App() {
   return (
     <div className="App">
       <Routes />
+      <button onClick={() => Mixpanel.track('test button')} > Mixpanel Test </button>
     </div>
   );
 }
