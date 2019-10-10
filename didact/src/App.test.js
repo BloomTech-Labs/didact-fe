4f9376624971ac221167e385f7869a6da86c9a76
Jonathan Scott <jonathan.scott@gmail.com>
2019-10-10 14:49:15 -0700

    adding testing, test for api data returning in the form of courses

--- didact/src/App.test.js
+++ didact/src/App.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom'
+import {render, waitForDomChange} from '@testing-library/react';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Router><App /></Router>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  // it('renders Hello World', () => {
+  //   const app = render(<Router><App /></Router>);
+
+  //   app.getByText(/App/i)
+  
+  // });
+  it('renders a course', () => {
+    // const container = document.createElement('div')
+    const container = render(<Router><App /></Router>);
+  waitForDomChange({ container })
+    .then(() => container.getByText(/Learning how to learn/i))
+    .catch(err => console.log(`Error you need to deal with: ${err}`))
+  // container.append(document.createElement('p'))
+// if 👆 was the only code affecting the container and it was not run,
+// waitForDomChange would throw an error
+  })
+})
