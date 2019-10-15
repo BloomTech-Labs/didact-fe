017fa7ef54509fa1dcb782fb627f5864555a35bc
Seth Nadu <seth.nadu@gmail.com>
2019-10-15 15:15:19 -0400

    tablet size

--- didact/src/components/headerAndFooter/Header.jsx
+++ didact/src/components/headerAndFooter/Header.jsx
@@ -115,7 +115,7 @@ const useStyles = makeStyles(theme => ({
     overflowX: "hidden",
     width: theme.spacing(7) + 1,
     [theme.breakpoints.up("sm")]: {
-      width: theme.spacing(8) + 1,
+      width: theme.spacing(8) + 1, 
       margin: "10px 10px 10px 10px",
       height: "800px",
       borderRadius: "15px"
