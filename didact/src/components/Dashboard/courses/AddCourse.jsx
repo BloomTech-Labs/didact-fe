e21136eaba65437d533d5103ce75657acde50528
Jonathan Scott <jonathan.scott@gmail.com>
2019-10-16 15:26:10 -0700

    form field styling mostly customized actions added for crud
    Co-authored-by: Seth Nadu <47128498+sethnadu@users.noreply.github.

--- didact/src/components/Dashboard/courses/AddCourse.jsx
+++ didact/src/components/Dashboard/courses/AddCourse.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
+import clsx from "clsx";
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
-import Divider from "@material-ui/core/Divider";
+import InputLabel from '@material-ui/core/InputLabel';
 
 const useStyles = makeStyles(theme => ({
+  
+  button: {
+    boxShadow: 'none',
+    borderRadius: '15px',
+    background: '#EBE8E1',
+    marginLeft: '70%',
+  },
   card: {
     maxWidth: 500,
     borderRadius: 15,
@@ -24,23 +32,51 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     flexWrap: 'wrap',
   },
+  borderRadius: {
+    borderRadius: "15px",
+  },
   titleOrInstructorFields: {
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
     width: '45%',
+    backgroundColor: '#F4F8FA',
+    filter: "brightness(107%)"
+  
   },
   descriptionField: {
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
     width: '93%',
-    height: '150px', 
+    height: '150px',
+    backgroundColor: '#F4F8FA',
+    filter: "brightness(107%)"
+  },
+  blackUnderline: {
+    '&:after': {
+      borderBottom: 'black solid 2px',
+      color: 'black',
+    },
   },
   courseUrlField: {
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
     width: '93%',
+    backgroundColor: '#F4F8FA',
+    filter: "brightness(107%)"
   }
 }));
+
+const labelStyle = {
+    color: 'black'
+}
+
+const CssTextField = withStyles({
+  root: {
+    '& label.Mui-focused': {
+      color: 'gray',
+    },
+  },
+})(TextField);
 
 export default function AddCourse() {
   const classes = useStyles();
@@ -55,8 +91,6 @@ export default function AddCourse() {
     setValues({ ...values, [name]: event.target.value });
   };
 
-
-
   return (
     <Card className={classes.card}>
       <CardContent>
@@ -64,47 +98,55 @@ export default function AddCourse() {
           Course Overview
         </Typography>
         <form className={classes.container} noValidate autoComplete="off">
-            <TextField
+            <CssTextField
                 id="standard-name"
-                label="Name"
+                label='Name'
                 className={classes.titleOrInstructorFields}
                 value={values.name}
                 onChange={handleChange('name')}
                 margin="normal"
-                variant="outlined"
+                variant="filled"
+                style={{onFocus: labelStyle}}
+                InputProps={{ classes: { underline: classes.blackUnderline } }}
+                // InputLabelProps={{ classes: {focused: classes.label}}}
+                // onFocus={style={labelStyle}}
             />
-            <TextField
+            <CssTextField
                 id="standard-name"
                 label="Instructors"
                 className={classes.titleOrInstructorFields}
-                value={values.name}
+                value={values.foreign_instructors}
                 onChange={handleChange('foreign_instructors')}
                 margin="normal"
-                variant="outlined"
+                variant="filled"
+                InputProps={{ classes: { underline: classes.blackUnderline } }}
             />
-            <TextField
+            <CssTextField
                 id="standard-name"
                 label="Description"
                 className={classes.descriptionField}
-                value={values.name}
+                value={values.description}
                 onChange={handleChange('description')}
                 margin="normal"
-                variant="outlined" 
+                multiline
+                rows='6'
+                variant="filled" 
+                InputProps={{ classes: { underline: classes.blackUnderline } }}
             />
-            <TextField
+            <CssTextField
                 id="standard-name"
                 label="Course Url"
                 className={classes.courseUrlField}
-                value={values.name}
+                value={values.link}
                 onChange={handleChange('link')}
                 margin="normal"
-                variant="outlined"
-  
+                variant="filled"
+                InputProps={{ classes: { underline: classes.blackUnderline } }}
             />
         </form>
       </CardContent>
       <CardActions>
-        <Button size="small">Add Course</Button>
+        <Button size="small" variant="contained" className={classes.button} >Add Course</Button>
       </CardActions>
     </Card>
   );
