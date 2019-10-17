d1f9d7f05194de012c74d3413ee8225486e38b50
Seth Nadu <seth.nadu@gmail.com>
2019-10-17 14:21:29 -0400

    fixed naming on folders

--- didact/src/App.js
+++ didact/src/App.js
@@ -3,15 +3,13 @@ import './App.css';
 import Routes from "./Routes";
 // import Header from './components/headerAndFooter/Header'
 import { Mixpanel } from './utils/mixpanel';
-import HeaderSecondary from './components/header/Header(No Icons)';
 
 
 function App() {
-
+  
   return (
     <div className="App">
       <Routes />
-      {/* <HeaderSecondary /> */}
       
       {/* <button onClick={() => Mixpanel.track('test button')} > Mixpanel Test </button> */}
     </div>
