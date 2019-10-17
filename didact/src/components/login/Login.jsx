1348cf23c99f65175aab8ce53e8e3882494d3d38
Ben <allenben@protonmail.com>
2019-10-17 15:04:04 -0500

    Cleaned up code

--- didact/src/components/login/Login.jsx
+++ didact/src/components/login/Login.jsx
@@ -4,30 +4,13 @@ import * as Yup from 'yup';
 import { loginAction } from '../../store/actions';
 import {useDispatch} from 'react-redux';
 
-import { makeStyles, withStyles } from '@material-ui/core/styles';
-import { mergeClasses } from "@material-ui/styles";
 
-const useStyles = makeStyles(theme => ({
-    header: {
-    
-    },
-    container: {
-       
-      },
-  }));
 
 const LoginForm = (props) => {
-    const classes = useStyles();
     const {errors, touched} = props
     return (
         <>
-        <div className={classes.header}>
-            <h1>Signup</h1>
-            <h4>Subtitle</h4>
-        </div>
-            <Form className={classes.container}>
-                <Field type= "name" name="firstName" placeholder="First Name"></Field>
-                <Field type= "name" name="lastName" placeholder="Last Name"></Field>
+            <Form>
                 <Field type= "email" name = "email" placeholder = "Email"></Field>
                 {touched.email && errors.email && <p>{errors.email}</p>}
                 <Field type= "password" name = "password" placeholder = "Password"></Field>
