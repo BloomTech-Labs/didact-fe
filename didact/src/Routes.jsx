1860a7162fb6e0f345f66f20c4acf3a71696ea7a
Jonathan Scott <jonathan.scott@gmail.com>
2019-10-22 17:22:06 -0700

    last push

--- didact/src/Routes.jsx
+++ didact/src/Routes.jsx
@@ -4,17 +4,14 @@ import FormikLoginForm from "./components/login/Login.jsx";
 import FormikRegisterForm from "./components/register/Register.jsx";
 import Auth from './auth/Auth';
 import MainPage from './components/mainPage/MainPage';
-import EditCourse from './components/courses/EditCourse';
 
 const Routes = () => {
-     
+
     return (
         <>
             <Route path="/login" component={FormikLoginForm} >
-                {/* {console.log(localStorage.getItem('token'))} */}
-                {!localStorage.getItem('token') ? null : <Redirect to='/dashboard' />}
+                {localStorage.getItem('token') ? <Redirect to='/dashboard' /> : null}
             </Route>
-            
             <Route path="/register" component={FormikRegisterForm} >
                 {localStorage.getItem('token') ? <Redirect to='/dashboard' /> : null}
             </Route>
