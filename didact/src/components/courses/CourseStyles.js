06d1a089b43b52d900e2d9777814c47dd7a60362
AndrewA0112 <andrew.mkal@outlook.com>
2019-11-05 17:47:13 -0600

    add existing course

---
+++ didact/src/components/courses/CourseStyles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const AddCourseToPath = styled.div`
+    top: 153px;
+    left: 694px;
+    width: 296px;
+    height: 334px;
+    border-radius: 20px;
+    opacity: 1;
+
+    .learningPathTitle {
+        display: flex;
+        border: black;
+        justify-content: space-between;
+    }
+`
