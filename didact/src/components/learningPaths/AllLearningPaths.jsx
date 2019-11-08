ec039fa32aa2ad6cdd121b1b4bb1a31422f1b37c
Jonathan Scott <jonathan.scott@gmail.com>
2019-11-08 13:58:43 -0800

    mobile scroll fix and join path fixed
    Co-authored-by: Seth Nadu <47128498+sethnadu@users.noreply.github.com>

--- didact/src/components/learningPaths/AllLearningPaths.jsx
+++ didact/src/components/learningPaths/AllLearningPaths.jsx
@@ -17,8 +17,8 @@ const AllLearningPaths = (props) => {
     const learningPaths = state.learningPathReducer.learningPaths
     const yourLearningPaths = state.learningPathReducer.yourLearningPaths
 
-    const joinPath = (e, order) => {
-        dispatch(joinLearningPath(e.target.id, props.props.history, order))
+    const joinPath = (id, order) => {
+        dispatch(joinLearningPath(id, props.props.history, order))
     }
 
     return (
@@ -30,7 +30,7 @@ const AllLearningPaths = (props) => {
                                 <div className='title'> 
                                     <h1>{learningPath.name}</h1>
                                     <div>
-                                        <button onClick={() => joinPath(yourLearningPaths.length)} id={learningPath.id}>Join Path</button>
+                                        <button onClick={() => joinPath(learningPath.id, yourLearningPaths.length)} id={learningPath.id}>Join Path</button>
                                     </div>
                                 </div>
                             </LearningPathCard>
