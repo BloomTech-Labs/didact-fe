cab7cd6b5681bd730fb4b45aa03af632c4584330
AndrewA0112 <andrew.mkal@outlook.com>
2019-11-12 16:36:15 -0600

    added profile to side list, fixed bug where courses were removed on reload

--- didact/src/components/drawer/SideListStyles.js
+++ didact/src/components/drawer/SideListStyles.js
@@ -5,4 +5,23 @@ export const SideListWrapper = styled.div`
     flex-direction: column;
     justify-content: space-between;
     height: 100%;
+
+    .profileSection {
+        width: 213px;
+        margin: 0 auto;
+        /* padding-top: 17px; */
+        /* padding-bottom: 24px; */
+        padding: 17px 15px 24px 15px;
+        border-top: #7070704A 1px solid;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        p.logout {
+
+            &:hover {
+                cursor: pointer;
+            }
+        }
+    }
 `
