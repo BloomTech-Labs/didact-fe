21c0466ec3f70af45bfb0aff64e495ff291ae354
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-02-25 17:58:23 -0500

    used makeStyles in Material-UI to change searchbar and implemented dropdown feature in searchbar

--- didact/src/components/mainPage/PageStyles.js
+++ didact/src/components/mainPage/PageStyles.js
@@ -1,48 +1,46 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const PageFlex = styled.div`
+  width: 100%;
+  display: flex;
+  /* width: auto; */
+
+  .drawer {
+    padding-right: 20px;
+  }
+
+  .headerMain {
+    /* padding-left: 50px; */
+    min-width: 826px;
     width: 100%;
-    display: flex;
-    /* width: auto; */
+    margin: 0 0 0 15px;
 
-    .drawer {
-        padding-right: 20px;
+    .header {
+      min-width: 825px;
+      width: 100%;
+      display: flex;
+      background: #ebe8e1;
+      border-radius: 19px;
+      margin-top: 10px;
+      justify-content: space-between;
+      align-items: center;
+      color: black;
+      padding: 10px 15px;
+
+      h2 {
+        margin: 0px 0px;
+        font-family: ITC Grouch;
+        font-size: 32px;
+      }
+
+      .navSection {
+        width: 100%;
+        max-width: 250px;
+        display: flex;
+        align-items: center;
+        justify-self: "flex-end";
+        justify-content: flex-end;
+      }
     }
-
-    .headerMain {
-        /* padding-left: 50px; */
-        min-width: 826px;
-        width : 100%;
-        margin: 0 0 0 15px;
-
-        .header {
-            min-width: 825px;
-            width: 100%;
-            display: flex;
-            background: #242424BF;
-            border-radius: 19px;
-            margin-top: 10px;
-            justify-content: space-between;
-            align-items: center;
-            color: #EBE8E1;
-            padding: 10px 15px;
-
-            h2 {
-                margin: 0px 0px;
-                font-family: ITC Grouch;
-                font-size: 32px;
-            }
-
-            .navSection {
-                width: 100%;
-                max-width: 250px;
-                display: flex;
-                align-items: center;
-                justify-self: "flex-end";
-                justify-content: flex-end;
-
-                
-            }
-        }
-    }
+  }
 `;
