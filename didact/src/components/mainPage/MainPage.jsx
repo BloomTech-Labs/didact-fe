235739b73e455e21617df0b22098462a26b3a6dc
Kodi <55065678+yutveg@users.noreply.github.com>
2020-03-02 10:12:28 -0800

    Update MainPage.jsx

--- didact/src/components/mainPage/MainPage.jsx
+++ didact/src/components/mainPage/MainPage.jsx
@@ -213,7 +213,7 @@ function MainPage(props) {
   };
 
   const handleSubmit = event => {
-    Mixpanel.track("Search Query")
+    //Mixpanel.track("Search Query")
     event.preventDefault();
     setResults(values);
     props.history.push("/results");
