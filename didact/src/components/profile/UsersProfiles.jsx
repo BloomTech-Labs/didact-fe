ca144b09cbce3140400af580cc23870fbf0cdaee
nicanor-alicaya <donalicaya@gmail.com>
2020-03-10 13:20:41 -0400

    working on UsersProfiles, not yet working

--- didact/src/components/profile/UsersProfiles.jsx
+++ didact/src/components/profile/UsersProfiles.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 
-import { editUser, deleteTool, getToolById } from "../../store/actions";
+import { editUser, getUsersProfiles } from "../../store/actions";
 
 import { DidactField, DidactInput, DidactLabel } from "../dashboard/FormStyles";
 import DeleteModal from "../courses/DeleteModal";
