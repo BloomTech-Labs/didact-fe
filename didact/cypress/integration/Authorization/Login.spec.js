3a915f22eeb2330850438887a21d790ec1d9a5c5
yutveg <kodi.0n31l@gmail.com>
2020-03-12 18:31:58 -0700

    register test added

--- didact/cypress/integration/Authorization/Login.spec.js
+++ didact/cypress/integration/Authorization/Login.spec.js
@@ -1,10 +1,9 @@
 describe("a user logging in to didact", () => {
 
     it("logs in a user", () => {
-        cy.visit('/landing')
+        cy.visit('/login')
         const email = "bob@bobmail.com"
         const password = "secretpass"
-        cy.get("button").first().click()
         cy.get("input").first().type(email).should('have.value', email)
         cy.get("input").last().type(password).should('have.value', password)
         cy.get("button").first().click();
