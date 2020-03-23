ca625a51367097ad496ccc94e75092284582212c
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-03-23 11:21:36 -0400

    setup myProfile styling for inputs and render

--- didact/src/components/profile/myProfileStyle.js
+++ didact/src/components/profile/myProfileStyle.js
@@ -32,14 +32,8 @@ export const ProfileAvatar = styled.div`
   justify-content: space-between;
 `;
 
-export const profileDivRender = styled.nav`
-  display: flex;
-  justify-content: space-between;
-  background: green;
-`;
-
 export const DiscordLinkDiv = styled.div`
-  margin: 5% 80% 0 0;
+  margin: 15% 80% 0 0;
   display: flex;
   width: 100%;
 
@@ -51,6 +45,58 @@ export const DiscordLinkDiv = styled.div`
 
     img {
       margin: 0 auto;
+    }
+
+    span {
+      padding-bottom: 100px;
+      float: right;
+      margin-top: 7%;
+    }
+  }
+`;
+
+export const DidactProfileButton = styled.button`
+  box-shadow: none;
+  border-radius: 7px;
+  color: #242424;
+  background: #ffffff;
+  border: 1px solid #242424;
+  height: 25px;
+  width: 123px;
+
+  cursor: pointer;
+  &:hover {
+    color: #ffffff;
+    background-color: #242424bf;
+    outline: none;
+  }
+`;
+
+export const EditProfileDiv = styled.div`
+  display: flex;
+
+  form {
+    display: flex;
+    flex-direction: column;
+
+    label {
+      font-family: Open Sans;
+      font-weight: bold;
+      font-size: 16px;
+      margin-bottom: 1%;
+    }
+
+    textarea {
+      height: 200px;
+      border-radius: 7px;
+      margin-bottom: 5%;
+    }
+
+    input {
+      height: 30px;
+      border-radius: 7px;
+      margin-bottom: 2%;
+      width: 35%;
     }
   }
 `;
