0c679aa6dfc591cb593bc02e08c8ddc952b41165
nicanor-alicaya <donalicaya@gmail.com>
2020-03-25 19:14:00 -0400

    reverted edit user page to an earlier code, fixed a bug when editing course section where the page is seemingly stuck after submitting edit.

--- didact/src/components/profile/EditUser.jsx
+++ didact/src/components/profile/EditUser.jsx
@@ -1,17 +1,4 @@
 import React, { useState, useEffect } from "react";
-// import { Form, Field } from "react-final-form";
-// import { TextField, Checkbox, Radio, Select } from "final-form-material-ui";
-import TextField from "@material-ui/core/TextField";
-import FormControl from "@material-ui/core/FormControl";
-import Checkbox from "@material-ui/core/Checkbox";
-import {
-  Button,
-  RadioGroup,
-  FormLabel,
-  MenuItem,
-  FormGroup,
-  FormControlLabel
-} from "@material-ui/core";
 
 import {
   DidactField,
@@ -19,7 +6,6 @@ import {
   DidactLabel,
   DidactTextArea
 } from "./ProfileStyles";
-import { makeStyles } from "@material-ui/core/styles";
 
 import { getUserById, editUser } from "../../store/actions";
 
@@ -28,27 +14,17 @@ import DeleteModal from "../courses/DeleteModal";
 import Card from "@material-ui/core/Card";
 import { useSelector, useDispatch } from "react-redux";
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex"
-  },
-  formControl: {
-    margin: theme.spacing(3)
-  }
-}));
-
 const EditUser = ({ props, id }) => {
   const dispatch = useDispatch();
   const state = useSelector(state => state);
   const user = state.onboardingReducer.user;
   const person = state.usersProfilesReducer.person;
   const [openModal, setOpenModal] = useState(false);
-  const classes = useStyles();
   const [changes, setChanges] = useState({
-    email: person.email,
-    owner: person.owner,
-    admin: person.admin,
-    moderator: person.moderator
+    email: "",
+    owner: "",
+    admin: "",
+    moderator: ""
   });
 
   useEffect(() => {
@@ -58,9 +34,9 @@ const EditUser = ({ props, id }) => {
   useEffect(() => {
     setChanges({
       email: person.email,
-      owner: "",
-      admin: "",
-      moderator: ""
+      owner: person.owner,
+      admin: person.admin,
+      moderator: person.moderator
     });
   }, [person]);
 
@@ -81,10 +57,6 @@ const EditUser = ({ props, id }) => {
   //     props.history.push("/articles");
   //   };
 
-  const handleChecked = event => {
-    setChanges({ ...state, [event.target.name]: event.target.checked });
-  };
-
   const handleSubmit = e => {
     e.preventDefault();
     dispatch(editUser(id, changes));
@@ -92,107 +64,45 @@ const EditUser = ({ props, id }) => {
   };
 
   return (
-    <div className={classes.root}>
-      <FormControl
-        className={classes.formControl}
-        noValidate
-        autoComplete="off"
-        onSubmit={handleSubmit}
-      >
-        <TextField
-          id="email"
-          label="Email"
-          type="email"
-          value={changes.email}
-          variant="filled"
-          onChange={handleChange}
-          name="email"
-        />
-        <div>
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={person.owner}
-                value={changes.owner}
-                onChange={handleChecked}
-                name="owner"
-              />
-            }
-            label="Owner"
+    <Card>
+      <form onSubmit={handleSubmit}>
+        <DidactField>
+          <DidactLabel>Email</DidactLabel>
+          <DidactInput
+            value={changes.email}
+            onChange={handleChange}
+            name="email"
           />
-        </div>
-        <div>
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={person.admin}
-                value={changes.admin}
-                onChange={handleChecked}
-                name="admin"
-              />
-            }
-            label="Admin"
+        </DidactField>
+        <DidactField
+          style={user.owner !== true ? { display: "none" } : { display: "" }}
+        >
+          <DidactLabel>Owner</DidactLabel>
+          <DidactInput
+            value={changes.owner}
+            onChange={handleChange}
+            name="owner"
           />
-        </div>
-        <div>
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={person.moderator}
-                value={changes.moderator}
-                onChange={handleChecked}
-                name="moderator"
-              />
-            }
-            label="Moderator"
+        </DidactField>
+        <DidactField>
+          <DidactLabel>Admin</DidactLabel>
+          <DidactInput
+            value={changes.admin}
+            onChange={handleChange}
+            name="admin"
           />
-        </div>
-        <div>
-          <Button type="submit" variant="contained" color="primary">
-            Submit
-          </Button>
-        </div>
-      </FormControl>
-    </div>
-    // <Card>
-    //   <form onSubmit={handleSubmit}>
-    //     <DidactField>
-    //       <DidactLabel>Email</DidactLabel>
-    //       <DidactInput
-    //         value={changes.email}
-    //         onChange={handleChange}
-    //         name="email"
-    //       />
-    //     </DidactField>
-    //     <DidactField
-    //       style={user.owner !== true ? { display: "none" } : { display: "" }}
-    //     >
-    //       <DidactLabel>Owner</DidactLabel>
-    //       <DidactInput
-    //         value={changes.owner}
-    //         onChange={handleChange}
-    //         name="owner"
-    //       />
-    //     </DidactField>
-    //     <DidactField>
-    //       <DidactLabel>Admin</DidactLabel>
-    //       <DidactInput
-    //         value={changes.admin}
-    //         onChange={handleChange}
-    //         name="admin"
-    //       />
-    //     </DidactField>
-    //     <DidactField>
-    //       <DidactLabel>Moderator</DidactLabel>
-    //       <DidactInput
-    //         value={changes.moderator}
-    //         onChange={handleChange}
-    //         name="moderator"
-    //       />
-    //     </DidactField>
-    //     <DidactButton type="submit">Submit</DidactButton>
-    //   </form>
-    // </Card>
+        </DidactField>
+        <DidactField>
+          <DidactLabel>Moderator</DidactLabel>
+          <DidactInput
+            value={changes.moderator}
+            onChange={handleChange}
+            name="moderator"
+          />
+        </DidactField>
+        <DidactButton type="submit">Submit</DidactButton>
+      </form>
+    </Card>
   );
 };
 
